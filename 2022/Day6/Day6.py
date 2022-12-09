2504f6a151b29c73e55cba991044ba9bee466a2b
Bo Mengels <12003586@student.pxl.be>
2022-12-09 09:39:01 +0100

    Day6

---
+++ 2022/Day6/Day6.py
@@ -0,0 +1,24 @@
+with open('input.txt', 'r') as f:
+    text = f.read().replace('\n', '')
+
+
+def find_start_of_packet(data: str, sequenceSize: int) -> int:
+    """ Returns the index of the start-of-packet marker """
+    for i in range(sequenceSize, len(data)):
+        sequence = data[i - sequenceSize: i]
+
+        found = False
+        for j in range(sequenceSize):
+            for k in range(j + 1, sequenceSize):
+                if sequence[j] == sequence[k]:
+                    found = True
+
+        if not found:
+            return i
+
+
+# Part 1
+print(f'Part 1 : {find_start_of_packet(text, 4)}')
+
+# Part 2
+print(f'Part 2 : {find_start_of_packet(text, 14)}')
